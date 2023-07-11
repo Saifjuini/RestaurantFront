9a149819253e0067de3caeba4c5546d8e21e500b
saif juini <saif.juini2@gmail.com>
2023-07-12 00:49:27 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:MainPageComponent },
+  {path : 'menu' , component :MenuComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutComponent } from './about/about.component';
+import { BookingComponent } from './booking/booking.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { ServiceComponent } from './service/service.component';
+import { TeamComponent } from './team/team.component';
+import { TestomonialComponent } from './testomonial/testomonial.component';
+import { FoodProductComponent } from './food-product/food-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AboutComponent,
+    BookingComponent,
+    ContactComponent,
+    MenuComponent,
+    ServiceComponent,
+    TeamComponent,
+    TestomonialComponent,
+    FoodProductComponent
   ],
   imports: [
     BrowserModule,
